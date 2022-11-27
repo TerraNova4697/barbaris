f1c6e1d06e3e5999abe337c894c4a981125bb098
nik <nikitabechthold@gmail.com>
2022-11-27 09:37:19 +0600

    Сделал навбар прозрачным сверху, белым при скролле. Изменил карточку основного блока

---
+++ js/navBar.js
@@ -0,0 +1,10 @@
+// const [red, green, blue, opac] = [255, 255, 255, 0.00]
+opac = 0.0;
+const section1 = document.querySelector(".menu-section");
+const MAX_OFFSET = 700;
+
+window.addEventListener("scroll", () => {
+  currPosY = window.scrollY < MAX_OFFSET ? window.scrollY : MAX_OFFSET;
+  opacity = currPosY / MAX_OFFSET;
+  section1.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+});
