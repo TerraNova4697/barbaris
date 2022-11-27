66ffff047e4e22734f70499f101c966f644cb5bf
nik <nikitabechthold@gmail.com>
2022-11-27 12:09:06 +0600

    мелкие изменения: слайдер, шрифты, этапы работ и т.д.

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,8 @@
 const sliderElm = document.querySelector(".slider-container .slider");
-const btnLeft = document.querySelector(".slider-container .btn-left");
-const btnRight = document.querySelector(".slider-container .btn-right");
+// const btnLeft = document.querySelector(".slider-container .btn-left");
+const btnLeft = document.querySelector(".testimonials .btn-left");
+// const btnRight = document.querySelector(".slider-container .btn-right");
+const btnRight = document.querySelector(".testimonials .btn-right");
 
 const numberSliderBoxs = sliderElm.children.length;
 let idxCurrentSlide = 0;
