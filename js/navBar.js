98df1d27fa563c36405292e79aad7bf227b6319a
nik <nikitabechthold@gmail.com>
2022-11-30 13:49:28 +0600

    Навигация готова

--- js/navBar.js
+++ js/navBar.js
@@ -5,6 +5,6 @@ const MAX_OFFSET = 700;
 
 window.addEventListener("scroll", () => {
   currPosY = window.scrollY < MAX_OFFSET ? window.scrollY : MAX_OFFSET;
-  opacity = currPosY / MAX_OFFSET;
+  opacity = currPosY / MAX_OFFSET + 0.3;
   section1.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
 });
