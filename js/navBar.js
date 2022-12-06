476a80b9dd67f93d2765f9105a281e0637b18443
nik <nikitabechthold@gmail.com>
2022-12-06 18:22:30 +0600

    навигация и лого

--- js/navBar.js
+++ js/navBar.js
@@ -1,10 +1,7 @@
 // const [red, green, blue, opac] = [255, 255, 255, 0.00]
-opac = 0.0;
-const section1 = document.querySelector(".menu-section");
-const MAX_OFFSET = 700;
-
-window.addEventListener("scroll", () => {
-  currPosY = window.scrollY < MAX_OFFSET ? window.scrollY : MAX_OFFSET;
-  opacity = currPosY / MAX_OFFSET + 0.3;
-  section1.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
-});
+function navigateTo(Y) {
+  window.scrollTo({
+    top: Y,
+    behavior: 'smooth'
+  });
+}
